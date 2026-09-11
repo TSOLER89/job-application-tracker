c3081ff97c7d100d25e72e2a10742c7230515f05
Tsoler Hayitian <tsoler.hayitian@gmail.com>
2026-09-11 08:24:40 +0200

    Initial React project setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <header className="header">
+        <h1>JobTrack</h1>
+        <p>Track your applications. Build your career.</p>
+      </header>
+
+      <main>
+        <h2>My Job Applications</h2>
+        <p>Your job applications will appear here.</p>
+      </main>
+    </div>
+  )
+}
+
+export default App
